4514588d8e55eae8d870eaa111bc69c5f6fbf2d7
Helena van Dorp <helena_dorp@hotmail.com>
2024-10-23 13:05:03 +0200

    [feature]:(select) add select and styles

---
+++ src/components/atoms/select/Component.tsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import styles from "./styles.module.css";
+import { localData } from "../../../utils/mockData/localData";
+
+export interface SelectProps {
+  variants?: any;
+  htmlFor?: string;
+  label: string;
+  placeholder?: string;
+  type?: string;
+  name?: string;
+  value?: string | number | readonly string[];
+  onBlur?: React.FocusEventHandler<HTMLSelectElement>;
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>;
+  id?: string;
+}
+
+const Select = ({ label, ...props }: SelectProps) => {
+  const Select = props.variants ? motion.select : "select";
+
+  return (
+    <Select className={styles.select} {...props}>
+      <option value="" disabled defaultValue={""}>
+        {label}
+      </option>
+      {localData.map((item, index) => (
+        <option key={index} value={item.name}>
+          {item.name}
+        </option>
+      ))}
+    </Select>
+  );
+};
+
+export default Select;
