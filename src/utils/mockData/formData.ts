3d1e318ff2f1bb9a49e3e1979f8df8eca60a9323
Helena van Dorp <helena_dorp@hotmail.com>
2024-10-23 12:59:43 +0200

    [chore]:(data) add initial data files

---
+++ src/utils/mockData/formData.ts
@@ -0,0 +1,41 @@
+export const formData = {
+  form: {
+    title: "Aanvraagformulier",
+    welcome: "Hi",
+    firstName: "Voornaam",
+    lastName: "Achternaam",
+    dateOfBirth: "Wat is je verjaardag?",
+    email: "Wat is je email?",
+    policy: {
+      basic: "Basis Plan",
+      medium: "Standaard Plan",
+      premium: "Premium Plan",
+    },
+    placeholder: {
+      firstName: "Voer hier je voornaam in",
+      lastName: "Voer hier je achternaam in",
+      mail: "Voer hier je mail in",
+      select: "Selecteer een optie..",
+    },
+    policyChoice: "Om welke verzekering gaat het?",
+    selectedPolicy: "Geselecteerde verzekering:",
+    errorMessage: {
+      firstName: "Voer hier je voornaam in",
+      lastName: "Voer hier je achternaam in",
+      mail: "Voer hier je mail in",
+      select: "Selecteer een optie..",
+    },
+    summary: {
+      check: "Zijn je gegevens correct?",
+    },
+  },
+  button: {
+    submitted: "Is verzonden",
+    edit: "Nee wijzig",
+    send: "Ja verstuur",
+    thanks: "Bedankt",
+  },
+  logo: {
+    alt: "logo Trustflow",
+  },
+};
---
+++ src/utils/mockData/localData.ts
@@ -0,0 +1,11 @@
+export const localData: Array<{ name: string; info: string }> = [
+  { name: "Basic Plan", info: "A minimal coverage policy" },
+  {
+    name: "Standard Plan",
+    info: "A moderate coverage policy with some additional benefits",
+  },
+  {
+    name: "Premium Plan",
+    info: "A comprehensive coverage policy with full benefits.",
+  },
+];
