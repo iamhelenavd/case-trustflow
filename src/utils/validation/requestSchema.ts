6a4e35bc824e034489989a2782e8838bb0a297ad
Helena van Dorp <helena_dorp@hotmail.com>
2024-10-23 20:19:28 +0200

    [feature]:(reqeustSchema): update schema

--- src/utils/validation/requestSchema.ts
+++ src/utils/validation/requestSchema.ts
@@ -3,43 +3,36 @@ import { localData } from "../mockData/localData";
 
 export const requestSchema = v.object({
   firstName: v.pipe(
-    v.string(""),
+    v.string(),
     v.nonEmpty("A.u.b. vul hier je voornaam in"),
     v.minLength(2, "Bevat minimum van 2 karakters"),
   ),
   lastName: v.pipe(
-    v.string(""),
+    v.string(),
     v.nonEmpty("A.u.b. vul hier je achternaam in."),
     v.minLength(2, "Bevat minimum van 2 karakters"),
   ),
-  // Date of Birth Validation
   dateOfBirth: v.pipe(
-    v.string(""),
+    v.string(),
     v.nonEmpty("A.u.b. vul hier je geboortedatum in."),
     v.custom((value) => {
-      // Log de waarde van de invoer om te zien wat 'birthday' is
-      console.log("Birthday input:", value);
 
       const today = new Date();
       const birthDate = new Date(value as string);
-
-      // Controleer of de datum geldig is en of de gebruiker minstens 18 jaar oud is
-      const isValidDate = !isNaN(birthDate.getTime()); // Zorg ervoor dat het een geldige datum is
+      const isValidDate = !isNaN(birthDate.getTime());
       const isAdult =
         today.getFullYear() - birthDate.getFullYear() > 18 ||
         (today.getFullYear() - birthDate.getFullYear() === 18 &&
           today.getMonth() >= birthDate.getMonth() &&
           today.getDate() >= birthDate.getDate());
 
-      // Return true als de datum geldig is en de gebruiker minstens 18 jaar oud is
-      return isValidDate && isAdult;
-    }, "Je moet minimaal 18 jaar zijn"), // Bied hier het foutbericht aan
+          return isValidDate && isAdult;
+    }, "Je moet minimaal 18 jaar zijn"),
   ),
   email: v.pipe(
-    v.string(""),
+    v.string(),
     v.nonEmpty("A.u.b. vul hier je email in."),
     v.email("Het email is verkeerd geformatteerd"),
-    v.maxLength(30, "Je email is te lang."),
   ),
   list: v.pipe(
     v.picklist(
